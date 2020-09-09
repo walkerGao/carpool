548237663cfdbdede718fcccabbc0344b056cd91
gaowei <>
2020-09-09 21:25:53 +0800

    carpool

---
+++ src/main/java/com/zxx/carpool/CarpoolApplication.java
@@ -0,0 +1,13 @@
+package com.zxx.carpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zxx/carpool/CarpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxx.carpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
